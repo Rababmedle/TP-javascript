09f491e96e1510cc95aafea427ee4e2286deaa80
Rababmedle <rabab.elmamy@gmail.com>
2023-10-18 18:02:01 +0200

    TP1

--- TP1.js
+++ TP1.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+//affichage
+console.log("hi");
+//les variable
+let var1 = "valeur1";
+console.log("Name:", var1);
+//constant
+const const1 = "test1";
+console.log("constant:", const1);
+//les type de données
