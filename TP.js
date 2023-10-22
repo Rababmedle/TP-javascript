cac380301d9ef169d11a6ab9b7e8b6dc7a122f33
Rababmedle <rabab.elmamy@gmail.com>
2023-10-22 11:51:59 +0200

    TP-GS

---
+++ TP.js
@@ -0,0 +1,18 @@
+//exo1
+//var
+var number = 20;
+console.log(number);
+//let
+let age = 4 * 5;
+console.log("Mon age est " + age);
+// const
+const titre = "hello";
+console.log(titre);
+function addnumber(a, b) {
+  return a + b;
+}
+//exo2 1)
+console.log(addnumber(2, 3));
+//arrow function
+const addnumber1 = (a, b) => a + b;
+console.log(addnumber);
