3c6a957db014229ce47400e556f5014d98b448dc
Rababmedle <rabab.elmamy@gmail.com>
2023-10-22 12:59:48 +0200

    TP-GS

--- TP.js
+++ TP.js
@@ -8,6 +8,7 @@ console.log("Mon age est " + age);
 // const
 const titre = "hello";
 console.log(titre);
+
 function addnumber(a, b) {
   return a + b;
 }
@@ -15,4 +16,13 @@ function addnumber(a, b) {
 console.log(addnumber(2, 3));
 //arrow function
 const addnumber1 = (a, b) => a + b;
-console.log(addnumber);
+console.log(addnumber1(4, 4));
+//exo5
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+console.log(person["firstName"]);
+console.log(person.firstName);
